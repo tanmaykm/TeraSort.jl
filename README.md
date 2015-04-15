8c2d873c848f09f6458f3906b8fd83232c9d2e3e
tan <tanmaykm@gmail.com>
2015-04-15 18:09:45 +0530

    preliminary version

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # TeraSort
 
-[![Build Status](https://travis-ci.org/tanmaykm/TeraSort.jl.svg?branch=master)](https://travis-ci.org/tanmaykm/TeraSort.jl)
--- src/TeraSort.jl
+++ src/TeraSort.jl
@@ -1,5 +1,17 @@
 module TeraSort
 
-# package code goes here
+using Elly
+using StatsBase
+
+export teragen, genrandfile
+export terasort, sortrandfile
+export terasample, samplerandfile
+export cleanup, startcluster, stopcluster
+
+include("util.jl")
+include("gen.jl")
+include("sort.jl")
+include("merge.jl")
+include("sample.jl")
 
 end # module
